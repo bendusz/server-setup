937fc048fae22f568396f9794a655ceb3cd1d529
ben.vegh <ben.vegh@autorek.com>
2023-02-10 16:38:30 +0000

    Install script

---
+++ setup.ps1
@@ -0,0 +1,8 @@
+### Download prerequisite for Terminal
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -outfile Microsoft.VCLibs.x86.14.00.Desktop.appx
+### Install pre-requisite for Terminal
+Add-AppxPackage Microsoft.VCLibs.x86.14.00.Desktop.appx 
+
+### Chocolatey script
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
+choco install microsoft-windows-terminal powershell-core dotnetfx vscode notepadplusplus googlechrome --confirm
