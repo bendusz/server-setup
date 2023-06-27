cf878467bd6621565d1394851c792aa6f4a2b031
Ben Vegh <55585710+bendusz@users.noreply.github.com>
2023-06-27 17:00:47 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,23 @@
-### Download prerequisite for Terminal
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -outfile Microsoft.VCLibs.x86.14.00.Desktop.appx
-### Install pre-requisite for Terminal
-Add-AppxPackage Microsoft.VCLibs.x86.14.00.Desktop.appx 
+# Provide URL to newest version of Windows Terminal Application
+$url = 'https://github.com/microsoft/terminal/releases/download/v1.17.11461.0/Microsoft.WindowsTerminal_1.17.11461.0_8wekyb3d8bbwe.msixbundle'
+$split = Split-Path $url -Leaf
+
+# Prerequisites
+# Check if it has been installed already
+if (-not (Get-AppxPackage | Where-Object { $_.Name -like "Microsoft.VCLibs.140.00.UWPDesktop" }) {
+  Start-BitsTransfer -Source 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx' `
+    -Destination $home\Microsoft.VCLibs.x86.14.00.Desktop.appx
+  Add-AppxPackage $home\Microsoft.VCLibs.x86.14.00.Desktop.appx
+}
+
+# Download
+Start-BitsTransfer `
+  -Source $url `
+  -Destination (Join-Path -Path $home -ChildPath $split)
+ 
+# Installation
+Add-AppxPackage -Path (Join-Path -Path $home -ChildPath $split)
 
 ### Chocolatey script
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
-choco install microsoft-windows-terminal powershell-core dotnetfx vscode notepadplusplus --confirm
+choco install powershell-core dotnetfx vscode notepadplusplus --confirm
