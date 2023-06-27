59ad7649667d5a3b68e7ac81a169ec2bc706ad1f
Ben Vegh <55585710+bendusz@users.noreply.github.com>
2023-06-27 12:50:02 +0100

    Create installNewADDS.ps1

---
+++ installNewADDS.ps1
@@ -0,0 +1,26 @@
+# Ensure PowerShell is running with elevated privileges
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Host "Please run this script with administrative privileges."
+    exit
+}
+
+# Install the Active Directory Domain Services and DNS Server roles
+Install-WindowsFeature -Name AD-Domain-Services, DNS -IncludeManagementTools
+
+# Specify the domain name and NetBIOS name for the new domain
+$domainName = Read-Host "Enter the domain name (like contoso.com)"
+$netBIOSName = Read-Host "Enter the domain netBIOS"
+
+# Specify the DSRM password for the Directory Services Restore Mode
+$dsrmPassword = Read-Host -AsSecureString -Prompt "Enter the DSRM password"
+
+# Configure the new forest and domain
+$credential = Get-Credential
+$adminPassword = $credential.Password | ConvertFrom-SecureString
+$adminUsername = "$netBIOSName\administrator"
+$domainMode = 7   # Windows Server 2019 domain functional level
+$forestMode = 7   # Windows Server 2019 forest functional level
+
+Install-ADDSForest -DomainName $domainName -DomainNetbiosName $netBIOSName -SafeModeAdministratorPassword $dsrmPassword -DatabasePath "C:\Windows\NTDS" -LogPath "C:\Windows\NTDS" -SysvolPath "C:\Windows\SYSVOL" -NoRebootOnCompletion:$false -Force:$true -CreateDNSDelegation:$false -DomainMode $domainMode -ForestMode $forestMode -InstallDns -Confirm:$false
+# Restart the server
+Restart-Computer -Force
