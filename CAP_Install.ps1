66364e2cd99ad59301ad60f073fc3c20aeef7671
Ben Vegh <55585710+bendusz@users.noreply.github.com>
2023-08-08 13:07:02 +0100

    Update CAP_Install.ps1

--- CAP_Install.ps1
+++ CAP_Install.ps1
@@ -1,2 +1,2 @@
 $VAR = Invoke-WebRequest https://raw.githubusercontent.com/bendusz/server-setup/main/CAPolicy.inf
-New-Item -Path %systemroot% -Name CAPolicy.inf -Value $VAR
+New-Item -Path "%systemroot%" -Name CAPolicy.inf -Value $VAR
