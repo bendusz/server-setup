786a64649c858198db0f8e51d2a2cbe5f9356817
Ben Vegh <55585710+bendusz@users.noreply.github.com>
2023-08-08 13:10:17 +0100

    Update CAP_Install.ps1
    Full Fix

--- CAP_Install.ps1
+++ CAP_Install.ps1
@@ -1,2 +1,6 @@
-$VAR = Invoke-WebRequest https://raw.githubusercontent.com/bendusz/server-setup/main/CAPolicy.inf
-New-Item -Path "%systemroot%" -Name CAPolicy.inf -Value $VAR
+$URL = "https://raw.githubusercontent.com/bendusz/server-setup/main/CAPolicy.inf"
+$VAR = Invoke-WebRequest -Uri $URL
+$Content = $VAR.Content
+
+$FilePath = Join-Path -Path $env:SystemRoot -ChildPath "CAPolicy.inf"
+$Content | Set-Content -Path $FilePath
